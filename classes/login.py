70da355a7d16cded0c2b52d5d2cfdc825c0ba8ca
Johnathon Kwisses <johnkwisses@gmail.com>
2017-05-15 11:07:45 -0600

    Add create_user to login.py.

--- classes/login.py
+++ classes/login.py
@@ -5,20 +5,26 @@ Note that all of the settings defined in a user's profile WILL OVERWRITE
 THE DEFAULT SETTINGS!
 """
 
+# Handles activity log
+import logging as log
+
 # Used to get number of user profiles
 from os import listdir
 
 
 class Login:
 
-    def __init__(self, settings):
+    def __init__(self, settings, ext=".yml"):
         """Handle login components.
         
         Args:
             settings (dict): Contains all settings.
         """
-        # Initialize parameters and instance variables
+        # Initialize parameters
         self.settings = settings
+        self.ext = ext
+
+        # Set instance variables
         self.users_path = self.settings["users_path"]
 
         # Get all user profiles
@@ -34,18 +40,44 @@ class Login:
         users = [user for user in listdir(self.users_path)]
         return users
 
+    def create_user(self):
+        """Create a user profile .yml file."""
+        # User enters required info below
+        print("Please provide your name and email below.")
+        name = input("Name: ").lower()
+        email = input("Email: ").lower()
+
+        # Set variables for new file
+        lines = ["name: {}\n".format(name), "email: {}\n".format(email)]
+        filepath = self.users_path + "/" + name + self.ext
+
+        # Write new file to filepath
+        try:
+            with open(filepath, "w+") as f:
+                f.writelines(lines)
+        except Exception as e:
+            log.error(e)
+
     def select_user(self):
         """Select one user for login."""
+        # Call create_user if no user exists
+        if not self.users:
+            self.create_user()
+            self.users = self.get_users()
+
+        # Select user
         if len(self.users) == 1:
             user = self.users[0].split(".")[0]
         else:
             print("Which user? ")
             user = input("> ").lower()
+
         return user
 
     def get_user_settings(self):
         """Get all user-defined program settings."""
-        with open(self.users_path + "/" + self.user + ".yml") as f:
+        filepath = self.users_path + "/" + self.user + self.ext
+        with open(filepath) as f:
             us = [setting.strip("\n") for setting in f.readlines()]
         return us
 
