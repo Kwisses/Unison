a22817a13b337ff22e265f24232d6f708f956812
Johnathon Kwisses <johnkwisses@gmail.com>
2017-06-22 22:33:07 -0600

    Add docstrings to windows_media_player.py.

--- modules/windows_media_player.py
+++ modules/windows_media_player.py
@@ -1,3 +1,5 @@
+"""WindowsMediaPlayer enables a user to play a song in Windows Media Player."""
+
 # To get and set desktop path and dir
 from os import path
 
@@ -11,22 +13,41 @@ from classes.module import Module
 class WindowsMediaPlayer(Module):
 
     def __init__(self):
+        """Set required inherited parameters."""
         super().__init__(name=WindowsMediaPlayer.__name__,
                          verbs=["play", "stop"])
         self.ext = ".mp3"
 
     def play(self, music_player, music):
+        """Open a process of music_player with music.
+        
+        Args:
+            music_player (str): Path to music player .exe file.
+            music (str): Music to play.
+        """
         if not self.process:
             self.process = Popen([music_player, music])
 
     def stop(self):
+        """Terminate instance of music_player."""
         if self.process:
             self.process.terminate()
+            self.process = None
 
     def run(self, **kwargs):
+        """Run module by set kwargs and verb switch statement.
+
+        Set using the instructions outlined in the inherited Module class.
+
+        Args:
+            **kwargs: 
+                settings (dict): All program settings.
+                verb (str): Action word to match with module.
+                noun (str): Item to be acted upon.
+        """
+        settings = kwargs["settings"]
         verb = kwargs["verb"]
         noun = kwargs["noun"]
-        settings = kwargs["settings"]
 
         music_player = settings["music_player"]
 
